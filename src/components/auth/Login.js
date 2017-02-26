2453e2d02ed93ec0cc94da9c851f3907827e5052
Seven <shiweidu@outlook.com>
2017-02-26 15:43:37 +0800

    完成登陆页UI

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -5,12 +5,101 @@
  */
 
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View, Text, TextInput, TouchableHighlight } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ececec'
+  },
+  navBar: {
+    paddingTop: 20,
+    height: 48,
+    backgroundColor: '#fff'
+  },
+  navBarContent: {
+    height: 48,
+    lineHeight: 48,
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '500'
+  },
+  textInput: {
+    backgroundColor: '#fff',
+    height: 56,
+    paddingRight: 16,
+    paddingLeft: 16
+  },
+  lineBox: {
+    height: 1,
+    backgroundColor: '#fff'
+  },
+  lineContext: {
+    height: 1,
+    position: 'relative',
+    left: 16,
+    backgroundColor: '#ececec'
+  },
+  loginBottomBox: {
+    marginTop: 24,
+    marginRight: 16,
+    marginBottom: 16,
+    marginLeft: 16,
+    height: 42,
+  },
+  loginButtomText: {
+    height: 42,
+    lineHeight: 42,
+    backgroundColor: '#ee5259',
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 16
+  }
+});
 
 class Login extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
+        <View style={styles.navBar}>
+          <Text style={styles.navBarContent}>登录</Text>
+        </View>
+
+        <TextInput
+          style={[styles.textInput, { marginTop: 48 }]}
+          placeholder="手机号"
+          autoCapitalize="none"
+          autoCorrect={false}
+          autoFocus={false}
+          keyboardType="numeric"
+          maxLength={11}
+        />
+
+        <View style={styles.lineBox}>
+          <View style={styles.lineContext} />
+        </View>
+
+        <TextInput
+          style={styles.textInput}
+          placeholder="密码"
+          autoCapitalize="none"
+          autoCorrect={false}
+          selectTextOnFocus={true}
+          secureTextEntry={true}
+        />
+
+         
+        <TouchableHighlight
+          style={styles.loginBottomBox}
+          onPress={() => {}}
+        >
+          <Text
+            style={styles.loginButtomText}
+          >
+            登录
+          </Text>
+        </TouchableHighlight>
+
       </View>
     );
   }
